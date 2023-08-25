eede9fa9f452d7ad2dadd2ed5f09144dc9b34365
Darren Dsouza <darrendsouza7273@gmail.com>
2023-08-25 20:17:14 +0530

    reactivness

--- carehub/src/components/Reminder/Reminder.js
+++ carehub/src/components/Reminder/Reminder.js
@@ -10,6 +10,7 @@ const Reminder = (props) => {
   const [title, setTitle] = useState('');
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
+  const [image, setImage] = useState(null); // Store the selected image file
   const [cards, setCards] = useState([]);
 
   function handleMenu() {
@@ -22,16 +23,35 @@ const Reminder = (props) => {
 
   function handleSubmit(event) {
     event.preventDefault();
+
+    // Create a new card object with the form data and the selected image
     const newCard = {
       title: title,
       name: name,
       description: description,
+      image: URL.createObjectURL(image), // Convert the selected image to a data URL
     };
+
     setCards([...cards, newCard]);
     setTitle('');
     setName('');
     setDescription('');
+    setImage(null); // Clear the selected image
   }
+
+  function handleImageChange(event) {
+    const selectedImage = event.target.files[0];
+    setImage(selectedImage);
+  }
+
+  function handleAddImageButtonClick() {
+    // Trigger the click event of the hidden file input element
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  }
+
+  const fileInputRef = React.createRef(); // Create a ref for the file input
 
   return (
     <div>
@@ -59,73 +79,92 @@ const Reminder = (props) => {
               </ul>
             </div>
           </div>
-          </main>
-          <div className="buttons">
-            <button className="button custom-button" onClick={toggleForm}>
-              Button 1
-            </button>
-            <button className="button button2">Button 2</button>
-          </div>
 
-          {isFormOpen && (
-            <form className="reminder-form" onSubmit={handleSubmit}>
-              <div className="form-group">
-                <label htmlFor="title">Title :</label>
-                <input
-                  type="text"
-                  id="title"
-                  name="title"
-                  value={title}
-                  onChange={(e) => setTitle(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="name">Name :</label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="description">Message :</label>
-                <textarea
-                  id="description"
-                  name="description"
-                  value={description}
-                  onChange={(e) => setDescription(e.target.value)}
-                  required
-                ></textarea>
-              </div>
-              <div className="form-group">
-                <button type="button" className="button custom-button">
-                  Add Image
-                </button>
-                <button type="button" className="button button2">
-                  Add Schedule
-                </button>
-              </div>
-
-              <button type="submit" className="submit-button">
-                Submit
+          {/* Container for Form and Cards */}
+          <div className="form-cards-container">
+            <div className="buttons">
+              <button className="button custom-button" onClick={toggleForm}>
+                Button 1
               </button>
-            </form>
-          )}
+              <button className="button button2">Button 2</button>
+            </div>
 
-          <div className="cards">
-            {cards.map((card, index) => (
-              <div className="card" key={index}>
-                <h2>{card.title}</h2>
-                <p>Name: {card.name}</p>
-                <p>Message: {card.description}</p>
-              </div>
-            ))}
+            {isFormOpen && (
+              <form className="reminder-form" onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <label htmlFor="title">Title :</label>
+                  <input
+                    type="text"
+                    id="title"
+                    name="title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="name">Name :</label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="description">Message :</label>
+                  <textarea
+                    id="description"
+                    name="description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    required
+                  ></textarea>
+                </div>
+                <div className="form-group">
+                  {/* Hidden file input element */}
+                  <input
+                    type="file"
+                    accept="image/*"
+                    ref={fileInputRef}
+                    style={{ display: 'none' }}
+                    onChange={handleImageChange}
+                  />
+                  {/* Combined button for Add Image and Choose File */}
+                  <button
+                    type="button"
+                    className="button custom-button"
+                    onClick={handleAddImageButtonClick}
+                  >
+                    Add Image / Choose File
+                  </button>
+                  <button type="button" className="button button2">
+                    Add Schedule
+                  </button>
+                </div>
+
+                <button type="submit" className="submit-button">
+                  Submit
+                </button>
+              </form>
+            )}
+
+            <div className="cards">
+              {cards.map((card, index) => (
+                <div className="card" key={index}>
+                  <img src={card.image} alt="Card" />
+                  <div className="card-content">
+                    <h2>{card.title}</h2>
+                    <p>Name: {card.name}</p>
+                    <p>Message: {card.description}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
-      
+        </main>
       </section>
     </div>
   );
