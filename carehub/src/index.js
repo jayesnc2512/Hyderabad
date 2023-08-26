32ae54f467b69c4957f666ac1f64db7aa2aeb67b
JNC <nilimajnc@gmail.com>
2023-08-26 09:12:12 +0530

    chat

--- carehub/src/index.js
+++ carehub/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
+
+
+Kommunicate.init("APP_ID", {
+  automaticChatOpenOnNavigation: true,
+  popupWidget: true
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -8,3 +15,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
